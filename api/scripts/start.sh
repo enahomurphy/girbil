fa5c4f9940b69ae51029e8b206ad7b3b52c5330f
Enaho Murphy <enahomurphy@gmail.com>
2020-03-23 10:19:07 +0100

    Reinstall files during deploy

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/girbil
+
+cp /home/ubuntu/.env /home/ubuntu/girbil
+
 sudo yarn migrate
 sudo yarn start
--- api/scripts/stop.sh
+++ api/scripts/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/girbil
 sudo pm2 stop server.config.js || exit 0
+
+rm -rf /home/ubuntu/girbil/ || exit 0

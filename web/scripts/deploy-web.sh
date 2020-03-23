6245367c674a9f9fddfbf4d523cb45527b257fdd
Enaho Murphy <enahomurphy@gmail.com>
2020-03-23 14:36:39 +0100

    Fix aws deploy failure

--- web/scripts/deploy-web.sh
+++ web/scripts/deploy-web.sh
@@ -2,7 +2,7 @@
 
 sudo yarn build-prod
 
-sudo aws s3 sync dist/ s3://$GIRBIL_WEB_STAGING_S3_CLIENT
+aws s3 sync dist/ s3://$GIRBIL_WEB_STAGING_S3_CLIENT
 
 # Break the CloudFront cache
-sudo aws cloudfront create-invalidation --distribution-id $GIRBIL_WEB_STAGING_CLOUNDFRONT_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id $GIRBIL_WEB_STAGING_CLOUNDFRONT_ID --paths "/*"

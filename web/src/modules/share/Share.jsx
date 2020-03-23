2a057b931fd2583edeb4cb2a3da6c23f82bb2ce4
Enaho Murphy <enahomurphy@gmail.com>
2020-03-23 21:05:08 +0100

    Share back button not working

--- web/src/modules/share/Share.jsx
+++ web/src/modules/share/Share.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/client';
 import { useCopyToClipboard } from 'react-use';
+import { useHistory } from 'react-router-dom';
 
 import Layout from '@/components/layout';
 import { Text, Flex } from '@/components/styles';
@@ -12,50 +13,63 @@ const Share = () => {
   const [state, copyToClipboard] = useCopyToClipboard();
   const { data } = useQuery(query.INVITE_URL);
   const url = get(data, 'inviteUrl', '');
+  const { goBack, push } = useHistory();
+
+  const closeWindow = () => {
+    push('/share');
+    window.close();
+  };
 
   return (
     <Layout title="Share an invite link">
-      <Text margin="16px 0 0 0">
-        Anyone can use this link to join Weave on Girbil,
-        so please only share with people you trust.
-      </Text>
-      {state.error && (
-      <p>
-        Unable to copy value:
-        {state.error.message}
-      </p>
-      )}
-      <Flex margin="32px 0 244px 0" direction="column" align="flex-start">
-        <Text color="#EFEFEF" weight="bold">
-          Invite link for Weave
+      <Fragment>
+        <Text margin="16px 0 0 0">
+          Anyone can use this link to join Weave on Girbil,
+          so please only share with people you trust.
         </Text>
-        <CopyForm margin="8px 0 0 0">
-          <input
-            onClick={() => copyToClipboard(url)}
-            value={url}
-            className="bordered"
-          />
-          <button
-            onClick={() => copyToClipboard(url)}
-            type="button"
-            className="green"
-          >
-            {state.value ? 'copied' : 'copy'}
-          </button>
-        </CopyForm>
-      </Flex>
+        {state.error && (
+        <p>
+          Unable to copy value:
+          {state.error.message}
+        </p>
+        )}
+        <Flex margin="32px 0 244px 0" direction="column" align="flex-start">
+          <Text color="#EFEFEF" weight="bold">
+            Invite link for Weave
+          </Text>
+          <CopyForm margin="8px 0 0 0">
+            <input
+              onClick={() => copyToClipboard(url)}
+              value={url}
+              className="bordered"
+              onChange={() => {}}
+            />
+            <button
+              onClick={() => copyToClipboard(url)}
+              type="button"
+              className="green"
+            >
+              {state.value ? 'copied' : 'copy'}
+            </button>
+          </CopyForm>
+        </Flex>
 
-      <button type="submit" className="primary">
-        back
-      </button>
-      <button
-        onClick={() => window.close()}
-        style={{ marginTop: '15px' }}
-        className="transparent"
-        type="button"
-      >
-        CLOSE BROWSER TAB
-      </button>
+        <button
+          onClick={goBack}
+          type="submit"
+          className="primary"
+        >
+          back
+        </button>
+        <button
+          onClick={closeWindow}
+          style={{ marginTop: '15px' }}
+          className="transparent"
+          type="button"
+        >
+          CLOSE BROWSER TAB
+        </button>
+      </Fragment>
     </Layout>
   );
 };

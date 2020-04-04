20de36c23fda506868c3b90e858e729a8a59b2e3
Enaho Murphy <enahomurphy@gmail.com>
2020-04-04 15:12:44 +0100

    Make electron configurable

---
+++ app/electron/windows/main.js
@@ -0,0 +1,61 @@
+const { join } = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const { logger } = require('../utils');
+const { browserWindow: options } = require('../config');
+
+// we use this method to allow users open the app from the web
+// only tested on safari and chrome
+// most of the code and inspiration was gooten form
+// https://github.com/oikonomopo/electron-deep-linking-mac-win
+const browserLink = (window) => {
+  const lock = app.requestSingleInstanceLock();
+
+  if (!lock) {
+    app.quit();
+    return;
+  }
+
+  // Someone tried to run a second instance, we should focus our window.
+  app.on('second-instance', (e, argv) => {
+    // Protocol handler for win32
+    // argv: An array of the second instance’s (command line / deep linked) arguments
+    if (process.platform === 'win32') {
+      // Keep only command line / deep linked arguments
+      const link = argv.slice(1);
+      logger(window, link);
+      // @TODO
+      // handle link for windows
+    }
+
+    if (window && window.isMinimized()) {
+      window.restore();
+      window.focus();
+    } else if (window) {
+      window.focus();
+    }
+  });
+};
+
+module.exports = () => {
+  let window = new BrowserWindow(options);
+
+  window.loadURL(join(`file://${__dirname}`, '../index.html'));
+
+  window.webContents.on('did-finish-load', () => {
+    window.webContents.send('window-id', window.id);
+  });
+
+  window.on('closed', () => {
+    window = null;
+  });
+
+  browserLink(window);
+
+  // Open the DevTools.
+  if (options.webPreferences.devTools) {
+    window.webContents.openDevTools();
+  }
+
+  return window;
+};

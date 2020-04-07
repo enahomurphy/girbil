dd1736d14379b8c49920ea564f2b83508abcbd82
Stephen Sunday <sunday@schrott24.at>
2020-04-07 23:51:20 +0100

    Add Error icon

---
+++ app/src/components/Icon/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Error = () => (
+  <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M9 9H8V6H9V9Z" fill="#FF4F44"/>
+    <path d="M15.5219 12.235L8.924 1.6784C8.72817 1.36506 8.27183 1.36506 8.076 1.6784L1.47812 12.235C1.26998 12.568 1.50941 13 1.90212 13H15.0979C15.4906 13 15.73 12.568 15.5219 12.235Z" stroke="#FF4F44"/>
+  </svg>
+);
+
+export default Error;

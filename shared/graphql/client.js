5e3ae197b8c3351ca4769fddc2d972ab982fc730
Stephen Sunday <sunday@schrott24.at>
2020-04-07 23:51:53 +0100

    Fix

--- shared/graphql/client.js
+++ shared/graphql/client.js
@@ -46,7 +46,8 @@ export default ({
     },
   }));
 
-  const httpLink = new HttpLink({ uri: `http://localhost:8081/graphql` });
+  const httpLink = new HttpLink({ uri: `${process.env.API_URL}/graphql` });
+
 
   const link = from([
     errrorLink,

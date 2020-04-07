a0ec8b683dc7a350eb597739cc49b0fb8b242d9c
Enaho Murphy <enahomurphy@gmail.com>
2020-04-07 23:50:55 +0100

    Improve back arrow

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -16,7 +16,16 @@ const Header = ({ title, backURL }) => (
       <Link onClick={
         () => {
           if (backURL) {
-            return f7.views.main.router.navigate(backURL, {
+            return f7.views.main.router.back(backURL, {
+              force: true,
+              reloadAll: true,
+            });
+          }
+
+          const noPrevious = f7.view.main.history.length === 1;
+
+          if (noPrevious) {
+            return f7.views.main.router.back('/conversations', {
               force: true,
               reloadAll: true,
             });
